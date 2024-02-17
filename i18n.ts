5b5a8448c8922ebab3b6d8e209a0074730fbc71e
Stefan Probst <stefanprobst@posteo.de>
2024-02-17 19:19:30 +0100

    remove not found

--- i18n.ts
+++ i18n.ts
@@ -1,13 +1,6 @@
-import "server-only";
-
-import { notFound } from "next/navigation";
 import { getRequestConfig } from "next-intl/server";
 
-import { isValidLocale } from "@/config/i18n.config";
-
 export default getRequestConfig(async ({ locale }) => {
-	if (!isValidLocale(locale)) notFound();
-
 	const _messages = await import(`@/messages/${locale}.json`)
 	const messages = _messages.default as IntlMessages;
 	const timeZone = "UTC";
