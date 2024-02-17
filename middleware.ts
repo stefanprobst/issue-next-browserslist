6768f0ec05ef57d1ddae122112f40553308f4da0
Stefan Probst <stefanprobst@posteo.de>
2024-02-17 11:05:58 +0100

    use latest stable next.js version 14.1.0

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,9 @@
-import type { MiddlewareConfig } from "next/server";
 import createI18nMiddleware from "next-intl/middleware";
 
 import { defaultLocale, locales } from "@/config/i18n.config";
 
 export default createI18nMiddleware({ defaultLocale, locales });
 
-export const config: MiddlewareConfig = {
+export const config = {
   matcher: ["/", "/(de|en)/:path*"],
 };
